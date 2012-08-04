be97984dfa26f3a1c3b57f5183e0f5cfa5a196ff
UENISHI Kota <kuenishi+github@gmail.com>
2012-08-04 23:47:24 +0900

    client test implemented: do TEST

--- src/msgpack_rpc_client.erl
+++ src/msgpack_rpc_client.erl
@@ -8,19 +8,42 @@
 %%%-------------------------------------------------------------------
 -module(msgpack_rpc_client).
 
--export([start_link/4, call/3, call_async/3, join/1, notify/3]).
+-include("msgpack_rpc.hrl").
 
-start_link(_, _, _, _)->
-    error.
+-export([start_link/4,
+	 connect/4, call/3, call_async/3, join/2, notify/3]).
 
-call(_, _, _)->
-    error.
+-type type()   :: tcp. % | udp | sctp.
+-type method() :: atom().
+-type argv()   :: [msgpack:msgpack_term()].
+-type callid() :: non_neg_integer().
 
-call_async(_, _, _)->
-    error.
+-spec start_link(type(), inet:ip_address(), inet:port_number(), [proplists:property()]) -> {ok, pid()} | {error, any()}.
+start_link(tcp, IP, Port, Opts)->
+    msgpack_rpc_connection:start_link([{transport, cowboy_tcp_transport}, {ipaddr, IP}, {port, Port}] ++ Opts);
+    % cowboy_tcp_transport:connect(IP, Port, Opts);
+start_link(_Type, _IP, _Port, _Opts)->
+    {error, no_transport}.
 
-join(_)->
-    error.
+-spec connect(atom(), inet:ip_address(), inet:port_number(), [proplists:property()]) -> {ok, pid()} | {error, any()}.
+connect(Name, IP, Port, Opts)->
+    start_link(Name, IP, Port, Opts).
 
-notify(_, _, _)->
-    error.
+-spec call(pid(), method(), argv()) -> {ok, msgpack:msgpack_term()} | {error, any()}.
+call(Pid, Method, Argv)->
+    case call_async(Pid, Method, Argv) of
+	{ok, CallID} -> join(Pid, CallID);
+	Error -> Error
+    end.
+
+-spec call_async(pid(), method(), argv()) -> {ok, callid()} | {error, any()}.
+call_async(Pid, Method, Argv) when is_atom(Method)->
+    gen_server:call(Pid, {call_async, Method, Argv}).
+
+-spec join(pid(), callid()) -> {ok, msgpack:msgpack_term()} | {error, any()}.
+join(Pid, CallID)->
+    gen_server:call(Pid, {join, CallID}).
+
+-spec notify(pid(), method(), argv()) -> ok. % never fails
+notify(Pid, Method, Argv)->
+    gen_server:cast(Pid, {notify, Method, Argv}).
